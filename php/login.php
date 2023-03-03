f874dbd1ffdf5384a2e6049b1a2110d34a8ef9f7
Farheen-21 <120936167+Farheen-21@users.noreply.github.com>
2023-03-03 17:51:34 +0530

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,36 @@
+<?php 
+$username = $_POST['username'];
+$password = $_POST['password'];
+$servername = "localhost:4306";
+$dbusername = "root"; // default username for XAMPP
+$dbpassword = ''; // default password for XAMPP is empty
+$dbname = "userdata"; // replace with your database name
+
+  $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+
+$user = mysqli_query($conn, "SELECT * FROM register WHERE username = '$username'");
+
+if(mysqli_num_rows($user) > 0){
+
+    $row = mysqli_fetch_assoc($user);
+
+    if($password == $row['passwordx']){
+      echo "Login Successful";
+      echo "<script> location.href='profile.html'; </script>";
+      exit;
+
+    
+    }
+    else{
+      echo "Wrong Password";
+      exit;
+    }
+  }
+  else{
+    echo "User Not Registered";
+    exit;
+  }
+
+
+?> 
